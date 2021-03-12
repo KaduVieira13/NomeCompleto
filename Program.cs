fcd35fd009445fff5e2d1444e245cc133986c18b
carlos eduardo <carloseduardo_alencar@outlook.com>
2021-03-12 10:42:38 -0300

    finaliza o projeto NomeCompleto

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace NomeCompleto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+         string nome;
+         string sobrenome;
+         string nomeCompleto;
+         string nomeCatalogo;
+
+         Console.WriteLine("------------------------------------------");
+         Console.WriteLine("|Veja seu nome em diferentes perspectivas.|");
+         Console.WriteLine("------------------------------------------");
+         Console.WriteLine();
+         Console.Write("Digite seu primeiro nome: ");
+         nome = Console.ReadLine();
+         
+         Console.Write("Digite seu sobrenome: ");
+         sobrenome = Console.ReadLine();
+         Console.WriteLine();
+         nomeCompleto = $"{nome} {sobrenome}";
+         nomeCatalogo = $"{sobrenome.ToUpper()}, {nome}";
+         Console.WriteLine($"Nome Completo: {nomeCompleto}");
+         Console.WriteLine($"Nome Catálogo: {nomeCatalogo}");
+        }
+    }
+}
